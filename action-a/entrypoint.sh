149d62368916d5bc1fe480d967add2edde790c78
Shivam Verma <sv805388@gmail.com>
2021-03-19 22:07:08 +0530

    added curl request in entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
+CT="Content-Type:application/json"
+
+TEST="curl http://127.0.0.1 -H $CT"
+echo $TEST
+
+RESPONSE=`$TEST`
+echo $RESPONSE
