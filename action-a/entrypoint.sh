c20569be4512b4d746d979be64d810b8459c76f0
Shivam verma <sv805388@gmail.com>
2021-08-12 22:03:10 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -3,7 +3,7 @@ apt-get update; apt-get --assume-yes install curl
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
 CT="Content-Type:application/json"
 
-TEST="curl http://398f4f144568.ngrok.io/push&type=push -H $CT"
+TEST="curl http://398f4f144568.ngrok.io/push?type=push -H $CT"
 echo $TEST
 
 RESPONSE=`$TEST`
